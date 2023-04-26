9a078b546c2b1b8ed9d26a0c4718d4a310b1e88a
a-weeb-programmer <austinortega@outlook.com>
2023-04-25 19:53:15 -0600

    Implemented Dispatch for the ScreencopyState.

--- src/wayland/screencopy/mod.rs
+++ src/wayland/screencopy/mod.rs
@@ -1,5 +1,13 @@
-use wayland_protocols_wlr::screencopy::v1::server::{zwlr_screencopy_frame_v1, zwlr_screencopy_manager_v1};
-use wayland_server::{backend::GlobalId, Dispatch, GlobalDispatch};
+//! Utilities for handling the `screencopy` protocol
+//!
+
+use wayland_protocols_wlr::screencopy::v1::server::{
+    zwlr_screencopy_frame_v1,
+    zwlr_screencopy_manager_v1::{self, Request},
+};
+use wayland_server::{
+    backend::GlobalId, protocol::wl_output::WlOutput, Dispatch, DisplayHandle, GlobalDispatch,
+};
 
 /// Screencopy global state
 #[derive(Debug)]
@@ -13,12 +21,42 @@ pub struct ScreencopyState {
 pub trait ScreencopyHandler {
     /// Gets the [`ScreencopyState`](crate::ScreencopyState) as a mutable reference
     fn screencopy_state(&mut self) -> &mut ScreencopyState;
+
+    /// The function to be executed when a client wants to capture an output
+    ///
+    /// Called when a client requests to capture the given `output`
+    /// with the `frame` that is ready for copying
+    fn capture_output(
+        &mut self,
+        frame: &zwlr_screencopy_frame_v1::ZwlrScreencopyFrameV1,
+        overlay_cursor: i32,
+        output: &WlOutput,
+    );
+}
+
+impl ScreencopyState {
+    /// Creates a new [`ScreencopyState`](self::Self)
+    ///
+    /// Your `D` type must have [`ScreencopyHandler`](self::ScreencopyHandler) implemented for it
+    /// and be delegated with [`delegate_screencopy`]
+    pub fn new<D>(display: &DisplayHandle) -> ScreencopyState
+    where
+        D: GlobalDispatch<zwlr_screencopy_manager_v1::ZwlrScreencopyManagerV1, ()>
+            + Dispatch<zwlr_screencopy_manager_v1::ZwlrScreencopyManagerV1, ()>
+            + Dispatch<zwlr_screencopy_frame_v1::ZwlrScreencopyFrameV1, ()>
+            + ScreencopyHandler
+            + 'static,
+    {
+        let id = display.create_global::<D, zwlr_screencopy_manager_v1::ZwlrScreencopyManagerV1, _>(3, ());
+        ScreencopyState { id }
+    }
 }
 
 impl<D> GlobalDispatch<zwlr_screencopy_manager_v1::ZwlrScreencopyManagerV1, (), D> for ScreencopyState
 where
     D: GlobalDispatch<zwlr_screencopy_manager_v1::ZwlrScreencopyManagerV1, ()>
         + Dispatch<zwlr_screencopy_manager_v1::ZwlrScreencopyManagerV1, ()>
+        + Dispatch<zwlr_screencopy_frame_v1::ZwlrScreencopyFrameV1, ()>
         + ScreencopyHandler
         + 'static,
 {
@@ -31,6 +69,73 @@ where
         data_init: &mut wayland_server::DataInit<'_, D>,
     ) {
         data_init.init(resource, ());
+    }
+}
+
+impl<D> Dispatch<zwlr_screencopy_manager_v1::ZwlrScreencopyManagerV1, (), D> for ScreencopyState
+where
+    D: GlobalDispatch<zwlr_screencopy_manager_v1::ZwlrScreencopyManagerV1, ()>
+        + Dispatch<zwlr_screencopy_manager_v1::ZwlrScreencopyManagerV1, ()>
+        + Dispatch<zwlr_screencopy_frame_v1::ZwlrScreencopyFrameV1, ()>
+        + ScreencopyHandler
+        + 'static,
+{
+    fn request(
+        state: &mut D,
+        _client: &wayland_server::Client,
+        _resource: &zwlr_screencopy_manager_v1::ZwlrScreencopyManagerV1,
+        request: <zwlr_screencopy_manager_v1::ZwlrScreencopyManagerV1 as wayland_server::Resource>::Request,
+        _: &(),
+        _dhandle: &DisplayHandle,
+        data_init: &mut wayland_server::DataInit<'_, D>,
+    ) {
+        match request {
+            Request::CaptureOutput {
+                frame,
+                overlay_cursor,
+                output,
+            } => {
+                let frame = data_init.init::<zwlr_screencopy_frame_v1::ZwlrScreencopyFrameV1, ()>(frame, ());
+                state.capture_output(&frame, overlay_cursor, &output);
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    fn destroyed(
+        _state: &mut D,
+        _client: wayland_server::backend::ClientId,
+        _resource: wayland_server::backend::ObjectId,
+        _data: &(),
+    ) {
+    }
+}
+
+impl<D> Dispatch<zwlr_screencopy_frame_v1::ZwlrScreencopyFrameV1, (), D> for ScreencopyState
+where
+    D: GlobalDispatch<zwlr_screencopy_manager_v1::ZwlrScreencopyManagerV1, ()>
+        + Dispatch<zwlr_screencopy_manager_v1::ZwlrScreencopyManagerV1, ()>
+        + Dispatch<zwlr_screencopy_frame_v1::ZwlrScreencopyFrameV1, ()>
+        + ScreencopyHandler
+        + 'static,
+{
+    fn request(
+        _state: &mut D,
+        _client: &wayland_server::Client,
+        _resource: &zwlr_screencopy_frame_v1::ZwlrScreencopyFrameV1,
+        _request: <zwlr_screencopy_frame_v1::ZwlrScreencopyFrameV1 as wayland_server::Resource>::Request,
+        _data: &(),
+        _dhandle: &DisplayHandle,
+        _data_init: &mut wayland_server::DataInit<'_, D>,
+    ) {
+    }
+
+    fn destroyed(
+        _state: &mut D,
+        _client: wayland_server::backend::ClientId,
+        _resource: wayland_server::backend::ObjectId,
+        _data: &(),
+    ) {
     }
 }
 
